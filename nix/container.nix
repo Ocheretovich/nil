d73445e14001e162c7242b90dd0f064944553b36
Konstantin Nazarov <mail@knazarov.com>
2025-03-12 19:03:55 +0000

    Add a nixos container that can run full cluster
    This PR when merged will allow you to run cluster setup almost exactly
    like it does in production, in a matter of seconds. The word
    "container" here may be a bit misleading, because even though we do
    start a cgroup, it will run systemd, nild, nginx, and other things
    like a normal operating system (and unlike Docker for example).
    
    Note, that you need NixOS installed and not just Nix package manager,
    because the `nixos-container` tool is not available with just Nix.
    
    ```
    sudo nixos-container create devcontainer --flake .
    sudo nixos-container start devcontainer
    ```
    
    If you made changes to nild or configs and want to update the
    container in-place, you can do:
    
    ```
    sudo nixos-container update devcontainer --flake .
    ```
    
    As a follow-up, I will add ClickHouse, exporter, and all frontend
    parts to run in the container as well.

---
+++ nix/container.nix
@@ -0,0 +1,161 @@
+{ pkgs, lib, config, ... }:
+with lib;
+let
+  solc = (pkgs.callPackage ./solc.nix { });
+  nil = (pkgs.callPackage ./nil.nix { solc = solc; });
+  devnetConfig = {
+    nild_config_dir = "etc/nild";
+    nild_credentials_dir = "etc/nild";
+    nild_p2p_base_tcp_port = 30303;
+    nil_wipe_on_update = true;
+    nil_rpc_port = 8529;
+    pprof_base_tcp_port = 6060;
+    nShards = 5;
+    nil_config = [
+      {
+        id = 0;
+        shards = [ 0 1 ];
+        splitShards = true;
+        dhtBootstrapPeersIdx = [ 0 2 3 ];
+      }
+      {
+        id = 1;
+        shards = [ 0 2 ];
+        splitShards = true;
+        dhtBootstrapPeersIdx = [ 0 1 3 ];
+      }
+      {
+        id = 2;
+        shards = [ 0 3 ];
+        splitShards = true;
+        dhtBootstrapPeersIdx = [ 0 1 2 ];
+      }
+      {
+        id = 3;
+        shards = [ 0 4 ];
+        splitShards = true;
+        dhtBootstrapPeersIdx = [ 0 1 2 ];
+      }
+    ];
+    nil_archive_config = [{
+      id = 0;
+      shards = [ 0 1 2 3 4 ];
+      bootstrapPeersIdx = [ 0 1 2 3 ];
+      dhtBootstrapPeersIdx = [ 0 1 2 3 ];
+    }];
+    nil_rpc_config = [{
+      id = 0;
+      dhtBootstrapPeersIdx = [ 0 1 2 3 ];
+      archiveNodeIndices = [ 0 ];
+    }];
+  };
+
+  format = pkgs.formats.yaml { };
+
+  configFiles = pkgs.stdenv.mkDerivation rec {
+    name = "devnet-configs";
+    src = format.generate "devnet.yaml" devnetConfig;
+    dontUnpack = true;
+    buildInputs = [ nil ];
+    buildPhase = ''
+      mkdir etc
+      nild gen-configs --basedir var/lib "$src"
+
+      base="$(pwd)"
+      find etc/nild/ -type f -name "*.yaml" -exec sed -i "s#$base/etc/nild#/etc/nild#g" {} +
+      find etc/nild/ -type f -name "*.yaml" -exec sed -i "s#$base/var/lib#/var/lib#g" {} +
+    '';
+    installPhase = ''
+      mkdir -p $out/etc
+      cp -r etc/nild/* $out/etc
+    '';
+  };
+in {
+  boot.isContainer = true;
+
+  networking.firewall.allowedTCPPorts = [ 80 ];
+
+  environment.systemPackages = [ nil configFiles pkgs.vim ];
+
+  environment.etc."nild".source = "${configFiles}/etc";
+
+  users.users.nil = {
+    isSystemUser = true;
+    group = "nil";
+  };
+  users.groups.nil = { };
+
+  systemd.services = builtins.listToAttrs (map (cfg: {
+    name = "nil-${toString cfg.id}";
+    value = {
+      description = "nil-${toString cfg.id} service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart =
+          "${nil}/bin/nild run -c /etc/nild/nil-${toString cfg.id}/nild.yaml";
+        Restart = "always";
+        User = "nil";
+        Group = "nil";
+        WorkingDirectory = "/var/lib/nil-${toString cfg.id}";
+        StateDirectory = "nil-${toString cfg.id}";
+        RuntimeDirectory = "nil-${toString cfg.id}";
+
+      };
+    };
+  }) devnetConfig.nil_config) //
+
+    (builtins.listToAttrs (map (cfg: {
+      name = "nil-rpc-${toString cfg.id}";
+      value = {
+        description = "nil-rpc-${toString cfg.id} service";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${nil}/bin/nild rpc -c /etc/nild/nil-rpc-${
+              toString cfg.id
+            }/nild.yaml";
+          Restart = "always";
+          User = "nil";
+          Group = "nil";
+          WorkingDirectory = "/var/lib/nil-rpc-${toString cfg.id}";
+          StateDirectory = "nil-rpc-${toString cfg.id}";
+          RuntimeDirectory = "nil-rpc-${toString cfg.id}";
+
+        };
+      };
+    }) devnetConfig.nil_rpc_config)) //
+
+    (builtins.listToAttrs (map (cfg: {
+      name = "nil-archive-${toString cfg.id}";
+      value = {
+        description = "nil-archive-${toString cfg.id} service";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${nil}/bin/nild archive -c /etc/nild/nil-archive-${
+              toString cfg.id
+            }/nild.yaml";
+          Restart = "always";
+          User = "nil";
+          Group = "nil";
+          WorkingDirectory = "/var/lib/nil-archive-${toString cfg.id}";
+          StateDirectory = "nil-archive-${toString cfg.id}";
+          RuntimeDirectory = "nil-archive-${toString cfg.id}";
+
+        };
+      };
+    }) devnetConfig.nil_archive_config));
+
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."default" = {
+      locations = { "/" = { proxyPass = "http://127.0.0.1:8529"; }; };
+      default = true;
+    };
+  };
+}
